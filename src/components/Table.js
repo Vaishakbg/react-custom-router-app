45a1cbeff103e2fe23b4f42f5b2f19b7d39c167f
Vaishakbg <vaishakbg@gmail.com>
2022-12-23 19:19:20 +0530

    Simple Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function Table() {
+function Table({ config, data, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={keyFn(column)}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="px-3 py-1" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
   return (
-    <div>Table</div>
-  )
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import Table from '../components/Table'
+import React from "react";
+import Table from "../components/Table";
 
 function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Cheery", color: "bg-red-500", score: 2.5 },
+  ];
+
+  const config = [
+    {
+      label: "Fruits",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: "Score Squared",
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
+    },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
   return (
     <div>
-        <Table />
+      <h2 className="text-xl mb-2">Simple Table</h2>
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
-  )
+  );
 }
 
-export default TablePage
+export default TablePage;
